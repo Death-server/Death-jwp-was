936286597c8fa456283aaf9d60d724a5a07a6767
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-12-20 19:53:39 +0900

    feat: HttpMethod.java HttpMethod enum 클래스

---
+++ src/main/java/HttpRequest/HttpMethod.java
@@ -0,0 +1,15 @@
+package HttpRequest;
+
+public enum HttpMethod {
+    GET("GET"), POST("POST");
+
+    private String method;
+
+    HttpMethod(String method) {
+        this.method = method;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+}
