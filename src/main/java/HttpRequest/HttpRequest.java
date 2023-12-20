14dcd3506994270a7091a8d3c469eadde5b53d7c
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-12-20 19:55:57 +0900

    feat: HttpRequest.java Http 입력을 저장하는 객체 - HttpRequest.java - RequestBody.java - RequestHeader.java

---
+++ src/main/java/HttpRequest/HttpRequest.java
@@ -0,0 +1,47 @@
+package HttpRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HttpRequest {
+    private static Logger logger = LoggerFactory.getLogger(HttpRequest.class);
+    private RequestHeader requestHeader;
+    private RequestBody requestBody;
+
+    private HttpRequest(RequestHeader requestHeader, RequestBody requestBody) {
+        logger.debug("Request Constructor");
+        this.requestHeader = requestHeader;
+        this.requestBody = requestBody;
+    }
+
+    public static HttpRequest of(String header) {
+        return new HttpRequest(new RequestHeader(header), new RequestBody());
+    }
+
+
+
+    public String getUrl() {
+        return requestHeader.getUrl();
+    }
+
+    public String getFileText() {
+        final String BASE_URL = "webapp";
+        try {
+            File file = new File(BASE_URL + requestHeader.getUrl());
+            if(file.isFile()) {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                return IOUtils.readData(br, (int) file.length());
+            }
+        } catch(IOException ioException) {
+            // 404 Not Found
+            logger.error(ioException.getMessage());
+        }
+        return null;
+    }
+}
---
+++ src/main/java/HttpRequest/RequestHeader.java
@@ -0,0 +1,42 @@
+package HttpRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.Parser;
+
+import java.security.InvalidParameterException;
+
+public class RequestHeader {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHeader.class);
+    private String version;
+    private HttpMethod method;
+    private String url;
+
+
+    public RequestHeader(String header) {
+
+        logger.debug("RequestHeader Constructor");
+        String[] parsedHeaders = Parser.parseRequestHeader(header);
+
+        final int PARSED_VERSION = 2;
+        final int PARSED_METHOD = 0;
+        final int PARSED_URL = 1;
+
+        this.version = parsedHeaders[PARSED_VERSION];
+        this.method = separateMethod(parsedHeaders[PARSED_METHOD]);
+        this.url = parsedHeaders[PARSED_URL];
+    }
+
+    private HttpMethod separateMethod(String method) {
+        if(method.equals(HttpMethod.GET.getMethod())) {
+            return HttpMethod.GET;
+        }
+
+        throw new InvalidParameterException("처리할 수 있는 메서드가 없음");
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+}
