4adc515e00e898fc32bcd00d45318f587ece76e1
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-12-20 20:52:13 +0900

    refactor: HttpRequest.java 데이터 읽어오는 책임을 IOUtils.java에 전가

--- src/main/java/util/IOUtils.java
+++ src/main/java/util/IOUtils.java
@@ -11,14 +11,7 @@ import java.io.IOException;
 public class IOUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(IOUtils.class);
-    /**
-     * @param BufferedReader는
-     *            Request Body를 시작하는 시점이어야
-     * @param contentLength는
-     *            Request Header의 Content-Length 값이다.
-     * @return
-     * @throws IOException
-     */
+
     public static String readData(BufferedReader br, int contentLength) throws IOException {
         char[] body = new char[contentLength];
         br.read(body, 0, contentLength);
