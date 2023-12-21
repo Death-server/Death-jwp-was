c487ea4fc63477eddc4db131db2140c6d4c0b6f4
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-12-21 17:13:37 +0900

    feat: SignUpController.java 회원가입 처리하는 로직 추가

---
+++ src/main/java/Controller/SignupController.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import HttpRequest.HttpRequest;
+import HttpResponse.HttpResponse;
+import Service.SignupService;
+import model.User;
+import util.IOUtils;
+
+public class SignupController implements Controller {
+
+    @Override
+    public void execute(HttpRequest httpRequest, HttpResponse httpResponse) {
+        User user = createUser(httpRequest);
+
+        SignupService.save(user);
+
+        httpResponse.addContext(IOUtils.getFileText("/index.html"));
+    }
+
+    private User createUser(HttpRequest httpRequest) {
+        final String USERID_KEY = "userId";
+        final String PASSWORD_KEY = "password";
+        final String NAME_KEY = "name";
+        final String EMAIL_KEY = "email";
+
+        return new User(httpRequest.getQueryValue(USERID_KEY),
+                httpRequest.getQueryValue(PASSWORD_KEY),
+                httpRequest.getQueryValue(NAME_KEY),
+                httpRequest.getQueryValue(EMAIL_KEY));
+    }
+
+}
