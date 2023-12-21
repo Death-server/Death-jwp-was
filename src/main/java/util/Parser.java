0544fc6d17aab3d5f7f103da27cb3270337afcbf
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-12-21 17:14:42 +0900

    refactor: Parser.java HttpRequestUtils.java 파일로 합침

--- src/main/java/util/Parser.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Parser {
-
-    private static final String BLANK = " ";
-    public static String[] parseRequestHeader(String header) {
-        return header.split(BLANK);
-    }
-}
