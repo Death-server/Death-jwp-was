e845c706bd4710c0f45aa958845ffc81f203664b
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2023-12-23 16:08:41 +0900

    feat: 이렇게 간단하게도 되네

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,16 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,13 +27,60 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            log.debug("request line : {}", line);
+            if (line == null) {
+                return;
+            }
+
+            String[] tokens = line.split(" ");
+
+            int contentLength = 0;
+            while (!line.isEmpty()) {
+                line = br.readLine();
+                log.debug("header : {}", line);
+                if(line.contains("Content-Length")) {
+                    contentLength = getContentLength(line);
+                }
+            }
+
+            String url = tokens[1];
+
+            if(("/user/create".equals(url))) {
+                String body = IOUtils.readData(br, contentLength);
+                log.debug(contentLength + "");
+                Map<String, String> params =
+                        HttpRequestUtils.parseQueryString(body);
+                User user = new User(params.get("userId"), params.
+                        get("password"), params.get("name"), params.get("email"));
+                log.debug("User : {}", user);
+                response302Header(new DataOutputStream(out), "/index.html");
+            } else if (url.startsWith("/user/create")) {
+                int index = url.indexOf("?");
+                String queryString = url.substring(index+1);
+                Map<String, String> params =
+                        HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.
+                        get("password"), params.get("name"), params.get("email"));
+                log.debug("User : {}", user);
+                response302Header(new DataOutputStream(out), "/index.html");
+            } else {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp" +
+                        tokens[1]).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private int getContentLength(String line) {
+        String[] headerTokens = line.split(":");
+        return Integer.parseInt(headerTokens[1].trim());
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -38,6 +88,16 @@ public class RequestHandler extends Thread {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String url) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Location: " + url + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
